4b76bc775aed46cd85dae2a39f7e2af62da9e2de
Empix <jotinhave@gmail.com>
2020-11-10 14:03:01 -0400

    Add publisher model and init

--- src/database/index.js
+++ src/database/index.js
@@ -5,6 +5,7 @@ const Engine = require('../models/Engine');
 const Game = require('../models/Game');
 const Genre = require('../models/Genre');
 const Developer = require('../models/Developer');
+const Publisher = require('../models/Publisher');
 
 const sequelize = new Sequelize(config);
 
@@ -12,6 +13,7 @@ Engine.init(sequelize);
 Game.init(sequelize);
 Genre.init(sequelize);
 Developer.init(sequelize);
+Publisher.init(sequelize);
 
 Engine.associate(sequelize.models);
 Game.associate(sequelize.models);
