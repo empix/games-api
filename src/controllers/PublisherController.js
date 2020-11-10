8e5b32a1980ae46b1332f525f2cc4259d73da3c9
Empix <jotinhave@gmail.com>
2020-11-10 14:28:08 -0400

    Create index function in publisher controller

--- src/controllers/PublisherController.js
+++ src/controllers/PublisherController.js
@@ -1,6 +1,12 @@
 const Publisher = require('../models/Publisher');
 
 module.exports = {
+  async index(req, res) {
+    const publishers = await Publisher.findAll();
+
+    return res.json(publishers);
+  },
+
   async store(req, res) {
     const { name } = req.body;
 
