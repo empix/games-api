607718ff2f9d8b92b8362d7348a8f9044c687592
Empix <jotinhave@gmail.com>
2020-11-13 16:15:41 -0400

    Create delete routes

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ const PublisherController = require('./controllers/PublisherController');
 
 router.get('/engines', EngineController.index);
 router.post('/engines', EngineController.store);
+router.delete('/engines/:id', EngineController.delete);
 
 router.get('/games/:id', GameController.findByPk);
 router.get('/games', GameController.index);
@@ -17,11 +18,14 @@ router.delete('/games/:id', GameController.delete);
 
 router.get('/genres', GenreController.index);
 router.post('/genres', GenreController.store);
+router.delete('/genres/:id', GenreController.delete);
 
 router.get('/developers', DeveloperController.index);
 router.post('/developers', DeveloperController.store);
+router.delete('/developers/:id', DeveloperController.delete);
 
 router.get('/publishers', PublisherController.index);
 router.post('/publishers', PublisherController.store);
+router.delete('/publishers/:id', PublisherController.delete);
 
 module.exports = router;
