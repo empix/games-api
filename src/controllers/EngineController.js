6db02228a6fb663cc6b1675ea33371d5af011966
Empix <jotinhave@gmail.com>
2020-11-13 15:27:22 -0400

    Add pagination feature

--- src/controllers/EngineController.js
+++ src/controllers/EngineController.js
@@ -2,7 +2,26 @@ const Engine = require('../models/Engine');
 
 module.exports = {
   async index(req, res) {
-    const engines = await Engine.findAll();
+    let { page, limit } = req.query;
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 10;
+    const offset = limit * (page - 1);
+
+    if (limit > 20 || limit < 1) {
+      return res
+        .status(400)
+        .json({ error: 'The limit must be between 1 and 20' });
+    }
+
+    if (page < 1) {
+      return res.status(400).json({ error: 'The page must be greater than 0' });
+    }
+
+    const engines = await Engine.findAll({
+      attributes: ['id', 'name'],
+      limit,
+      offset,
+    });
 
     return res.json(engines);
   },
