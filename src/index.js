172ad0d7776aa767dacdcc994f692620b51b78b1
Empix <jotinhave@gmail.com>
2020-11-24 19:10:24 -0400

    Add cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 3333;
 
 const morgan = require('morgan');
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 require('./database/index');
@@ -10,6 +11,7 @@ require('./database/index');
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routes);
